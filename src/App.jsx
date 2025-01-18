80e7d6436f04825f10527b4f38ea915a0aff4f67
butch888 <never_man@mail.ru>
2025-01-19 01:07:46 +0300

    Added folder useFetch whith useFetch.js and UseFetchDemo.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,11 @@
 import './App.css';
-import  useFetch  from './CustomHooks/useFetch';
-
-function Demo() {
-  const {
-    data,
-    isLoading,
-    error,
-    refetch
-  } = useFetch('https://jsonplaceholder.typicode.com/posts');
-	
-  return (
-    <div>
-      <div>
-        <button onClick={() => refetch({
-          params: {
-            _limit: 3
-          }
-        })}>
-          Перезапросить
-        </button>
-      </div>
-      {isLoading && 'Загрузка...'}
-      {error && 'Произошла ошибка'}
-      {data && !isLoading && data.map(item => <div key={item.id}>{item.id}</div>) }
-    </div>
-  );
-}
-
+import UseFetchDemo from './CustomHooks/useFetch/UseFetchDemo';
 
 function App() {
   return (
     <div>
       <h1>Title</h1>
-      <Demo/>
+      <UseFetchDemo />
     </div>
   )
 }
---
+++ src/CustomHooks/useFetch/UseFetchDemo.jsx
@@ -0,0 +1,29 @@
+import useFetch from "./useFetch";
+
+function UseFetchDemo() {
+  const {
+    data,
+    isLoading,
+    error,
+    refetch
+  } = useFetch('https://jsonplaceholder.typicode.com/posts');
+	
+  return (
+    <div>
+      <div>
+        <button onClick={() => refetch({
+          params: {
+            _limit: 3
+          }
+        })}>
+          Перезапросить
+        </button>
+      </div>
+      {isLoading && 'Загрузка...'}
+      {error && 'Произошла ошибка'}
+      {data && !isLoading && data.map(item => <div key={item.id}>{item.id}</div>) }
+    </div>
+  );
+}
+
+export default UseFetchDemo
