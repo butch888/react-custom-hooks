d1b1319a5228c49e642d05b2732ffd6371212323
butch888 <never_man@mail.ru>
2025-01-25 20:48:40 +0300

    Added useHover hook

---
+++ src/CustomHooks/UseHoverDemo.jsx
@@ -0,0 +1,13 @@
+import { useHover } from "./useHover";
+
+function UseHoverDemo() {
+  const { hovered, ref } = useHover();
+
+  return (
+    <div ref={ref}>
+      {hovered ? 'На меня навели мышку' : 'Наведи мышкой на меня'}
+    </div>
+  );
+}
+
+export default UseHoverDemo;
